c962c79e5558305cbc5c55423b5f5d1e6d899ea3
stayasleep <virtuallyinfekted@gmail.com>
2017-10-05 16:02:17 -0700

    rudimentary code split using System.import(file).then()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {addToDom} from './helper';
 import add from './add';
 
+
 function addHeader(){
 	var header = document.createElement('h1');
 	header.innerText = "Vanilla JS ftw";
@@ -13,8 +14,23 @@ function addDiv(){
 	return div
 }
 
+function addBtn(){
+	var btn = document.createElement('button');
+	btn.innerText = "Click to subtract";
+	btn.onclick = function(){
+		console.log('Alas poor yorick, i knew him, Horatio!');
+		//this makes an async call and returns a promise
+		System.import('./subtract').then(function(module){
+		    console.log('27-4 = ',module.default(27,12));
+		});
+
+	}
+	return btn;
+}
+
 
 addToDom(addHeader());
 addToDom(addDiv());
+addToDom(addBtn());
 
 
